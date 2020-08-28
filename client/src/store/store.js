f21647cc2c628cae19e276a1328772c3bdee8a6e
Joël Grimberg <jgrimberg@xebia.com>
2020-08-28 10:50:37 +0200

    fix

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -28,10 +28,10 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    setToken ({commit}, token) {
+    setToken ({ commit }, token) {
       commit('setToken', token)
     },
-    setUser ({commit}, user) {
+    setUser ({ commit }, user) {
       commit('setUser', user)
     }
   }

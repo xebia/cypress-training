57f7057d081157eeb5b439436e6732b7101aa940
rahik.tamim <rahik.tamim@eurail.com>
2023-12-01 16:10:40 +0100

    changes name

--- client/tests/example.spec.ts
+++ client/tests/example.spec.ts
@@ -7,7 +7,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started links', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
